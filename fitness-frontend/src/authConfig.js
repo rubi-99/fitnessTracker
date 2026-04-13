f516d4a6f04513aedd11ae49287b4a6444a9246b
rubi-99 <adhikarirubi101@gmail.com>
2026-04-13 20:14:05 +0530

    front end deleted

--- fitness-frontend/src/authConfig.js
+++
@@ -1,8 +0,0 @@
-export const authConfig= {
-  clientId: 'oauth2-new',
-  authorizationEndpoint: 'http://localhost:8181/realms/master/protocol/openid-connect/auth',
-  tokenEndpoint: 'http://localhost:8181/realms/master/protocol/openid-connect/token',
-  redirectUri: 'http://localhost:5173',
-  scope: 'openid profile email offline_access',
-  onRefreshTokenExpire: (event) => event.login(),
-}
